f8a5d91b662a5fadaf67c09b92b959df7841f748
lpshanley <lpshanley@gmail.com>
2016-10-09 17:53:04 -0400

    Added logic to log out requested data

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 $( document ).ready(function(){
 	
-	var gitObj = {};
-	
 	$.ajax({
 		url: 'https://api.github.com/repos/lpshanley/mnsure-script-engine/branches',
 		async: false
 	}).success(function( data ){
 		
-		gitObj = data;
+		$.each(data,function(k,v){
+			
+			console.log( `Name: ${data.name}` );
+			console.log( `Sha: ${ data.commit.sha.substring((data.commit.sha.length - 7), data.commit.sha.length) }` );
+			
+		});
+		
 		
 	});
-
-	console.log( gitObj );
 	
 });
