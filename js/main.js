a2279db0cc08872870004d45068959091e72935a
lpshanley <lpshanley@gmail.com>
2016-10-09 17:55:08 -0400

    Debugging success

--- js/main.js
+++ js/main.js
@@ -5,13 +5,16 @@ $( document ).ready(function(){
 		async: false
 	}).success(function( data ){
 		
+		console.log( data );
+		
+		/*
 		$.each(data,function(k,v){
 			
 			console.log( `Name: ${data.name}` );
 			console.log( `Sha: ${ data.commit.sha.substring((data.commit.sha.length - 7), data.commit.sha.length) }` );
 			
 		});
-		
+		*/
 		
 	});
 	
