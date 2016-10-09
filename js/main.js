d6d5a333ae26080c927286d2907150a5908bd20a
lpshanley <lpshanley@gmail.com>
2016-10-09 17:39:06 -0400

    Added ajax to git

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $( document ).ready(function(){
+	
+	var gitObj = {};
+	
+	$.ajax({
+		url: 'https://api.github.com/repos/lpshanley/mnsure-script-engine/branches',
+		async: false,
+		success: function(data){
+			
+			gitObj = data;
+			
+		}
+	});
 
-	$('#output').html('<span>Working and loaded...</span>');
-
+	console.log( gitObj );
+	
 });
