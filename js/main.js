35b1d48f6ef85636a0e8d52870a77fee8689c641
lpshanley <lpshanley@gmail.com>
2016-10-09 18:21:25 -0400

    Added create card function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $( document ).ready(function(){
 	
+	var addCard = function( data ){
+		
+		var div = $( '<div>',{ 'class':'card' });
+		
+		$( 'body' ).append( div );
+		
+	}
+	
 	$.ajax({
 		url: 'https://api.github.com/repos/lpshanley/mnsure-script-engine/branches',
 		async: true
